0235271491b39c4b4cf66e82e959440da13e83e5
sunjinjie <sunjinjie@sunjinjiedemac.local>
2018-12-18 10:58:42 +0800

    13-2

--- Web/ViewController.swift
+++ Web/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var urlTF: UITextField!
+    @IBOutlet weak var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let url = URL(string: "http://www.163.com"){
+            webView.loadRequest(URLRequest(url: url))
+        }
     }
-
+    
+    @IBAction func back(_ sender: Any) {
+        webView.goBack()
+    }
+    
+    @IBAction func forward(_ sender: Any) {
+        webView.goForward()
+    }
+    
+    @IBAction func reload(_ sender: Any) {
+        webView.reload()
+    }
+    
+    @IBAction func go(_ sender: Any) {
+        if let url = URL(string: urlTF.text!){
+            webView.loadRequest(URLRequest(url: url))
+        }
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
